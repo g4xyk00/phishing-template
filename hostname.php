5e5e56712b4f53067d37405e30501534c977d3b0
Gary Κ <33410501+g4xyk00@users.noreply.github.com>
2019-06-20 09:46:39 +0800

    Update hostname.php

--- hostname.php
+++ hostname.php
@@ -12,10 +12,18 @@ function getUserIpAddr(){
 
 date_default_timezone_set("Asia/Kuala_Lumpur");
 $DATE_TIME = date('d-m-Y H:i:s');
+
+if(isset($_GET["email"])){
+	$EMAIL = $_GET["email"];
+}else{
+	$EMAIL = "";
+}
+
+$IP_ADDR = "";
 $IP_ADDR = getUserIpAddr();
 $HOST_NAME = gethostbyaddr($IP_ADDR);
-$LOG_INFO = $IP_ADDR.' ('.$HOST_NAME.') accessed at '.$DATE_TIME."\r\n";
-$LOG_PATH = "data.txt";
+$LOG_INFO = $IP_ADDR.' ('.$HOST_NAME.') with '.$EMAIL.' accessed at '.$DATE_TIME."\r\n";
+$LOG_PATH = "8d777f385d3dfec8815d20f7496026dc.txt";
 $LOG_FILE = fopen($LOG_PATH, 'a+');
 fwrite($LOG_FILE, $LOG_INFO);
 fclose($LOG_FILE);
